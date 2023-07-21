bead59cdd0619668cb2c818c83d4cfaf3c11e9cb
“Pawank06” <pawan06kumar2003@gmail.com>
2023-07-21 18:34:18 +0530

    created the frontend

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Board from './tic-tac-toe/Board';
 
 function App() {
-  return (
-    <div className="App">
-     <h1> tic tac toe !!</h1>
+  return(
+    <div>
+      <Board />
     </div>
-  );
+  )
 }
 
 export default App;
